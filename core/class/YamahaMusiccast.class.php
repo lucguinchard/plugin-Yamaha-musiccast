577af02614a95df172a26be9cab3a3fa90e8c280
Luc Guinchard <luc@guinchard.org>
2020-01-20 11:40:41 +0100

    Création des commandes dans postSave

--- core/class/YamahaMusiccast.class.php
+++ core/class/YamahaMusiccast.class.php
@@ -79,36 +79,6 @@ class YamahaMusiccast extends eqLogic {
 			$jsonGetDeviceInfo = YamahaMusiccast::CallAPI("GET", $this, "/YamahaExtendedControl/v1/system/getDeviceInfo");
 			$getDeviceInfo = json_decode($jsonGetDeviceInfo);
 			$this->setConfiguration('model_name', $getDeviceInfo->model_name);
-
-			$jsonGetFeatures = YamahaMusiccast::CallAPI("GET", $this, "/YamahaExtendedControl/v1/system/getFeatures");
-			$getFeatures = json_decode($jsonGetFeatures);
-			foreach ($getFeatures->zone as $zone) {
-				$zoneName = $zone->id;
-				foreach ($zone->func_list as $func) {
-					$this->createCmd($zoneName. '_' .$func . '_state');
-				}
-				$this->createCmd($zoneName. '_audio_error');
-				$this->createCmd($zoneName. '_audio_format');
-				$this->createCmd($zoneName. '_audio_fs');
-			}
-
-			$this->createCmd('netusb_input');
-			$this->createCmd('netusb_play_queue_type');
-			$this->createCmd('netusb_playback');
-			$this->createCmd('netusb_repeat');
-			$this->createCmd('netusb_shuffle');
-			$this->createCmd('netusb_play_time');
-			$this->createCmd('netusb_total_time');
-			$this->createCmd('netusb_artist');
-			$this->createCmd('netusb_album');
-			$this->createCmd('netusb_track');
-			$this->createCmd('netusb_albumart_url');
-			$this->createCmd('netusb_albumart_id');
-			$this->createCmd('netusb_usb_devicetype');
-			$this->createCmd('netusb_usb_auto_stopped');
-			$this->createCmd('netusb_attribute');
-			$this->createCmd('netusb_repeat_available');
-			$this->createCmd('netusb_shuffle_available');
 		}
 	}
 
@@ -129,6 +99,34 @@ class YamahaMusiccast extends eqLogic {
 	public function postSave() {
 		$jsonGetFeatures = YamahaMusiccast::CallAPI("GET", $this, "/YamahaExtendedControl/v1/system/getFeatures");
 		$getFeatures = json_decode($jsonGetFeatures);
+		foreach ($getFeatures->zone as $zone) {
+			$zoneName = $zone->id;
+			foreach ($zone->func_list as $func) {
+				$this->createCmd($zoneName. '_' .$func . '_state');
+			}
+			$this->createCmd($zoneName. '_audio_error');
+			$this->createCmd($zoneName. '_audio_format');
+			$this->createCmd($zoneName. '_audio_fs');
+		}
+
+		$this->createCmd('netusb_input');
+		$this->createCmd('netusb_play_queue_type');
+		$this->createCmd('netusb_playback');
+		$this->createCmd('netusb_repeat');
+		$this->createCmd('netusb_shuffle');
+		$this->createCmd('netusb_play_time');
+		$this->createCmd('netusb_total_time');
+		$this->createCmd('netusb_artist');
+		$this->createCmd('netusb_album');
+		$this->createCmd('netusb_track');
+		$this->createCmd('netusb_albumart_url');
+		$this->createCmd('netusb_albumart_id');
+		$this->createCmd('netusb_usb_devicetype');
+		$this->createCmd('netusb_usb_auto_stopped');
+		$this->createCmd('netusb_attribute');
+		$this->createCmd('netusb_repeat_available');
+		$this->createCmd('netusb_shuffle_available');
+		
 		foreach ($getFeatures->zone as $zone) {
 			YamahaMusiccast::callZoneGetStatus($this, $zone->id);
 		}

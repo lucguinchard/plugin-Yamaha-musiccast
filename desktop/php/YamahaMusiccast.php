71a8f3ce65dbda00ed92da867436990ee6d4be72
Luc Guinchard <luc@guinchard.org>
2021-11-29 10:41:10 +0100

    Permet d’avoir les images régulières.

--- desktop/php/YamahaMusiccast.php
+++ desktop/php/YamahaMusiccast.php
@@ -53,9 +53,10 @@ $eqLogicList = eqLogic::byType($plugin->getId());
 				foreach ($eqLogicList as $eqLogic) {
 					$opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard'; ?>
 					<div class="eqLogicDisplayCard cursor <?= $opacity ?>" data-eqLogic_id="<?= $eqLogic->getId() ?>">
-					<img src="<?= $eqLogic->getImage() ?>" />
-					<br/>
-					<span class="name"><?= $eqLogic->getHumanName(true, true) ?></span>
+						<div style="height: 100px">
+							<img src="<?= $eqLogic->getImage() ?>" style="max-width: 100px !important;width: auto !important;max-height: 100px !important;"/>
+						</div>
+						<span class="name"><?= $eqLogic->getHumanName(true, true) ?></span>
 					</div>
 				<?php } ?>
 			</div>
